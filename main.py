71951cc387845c45492d0aa977a11ccc1eb9d8dc
47985924 <luke.haoyi@gmail.com>
2026-05-12 23:26:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,42 @@
-##################### Extra Hard Starting Project ######################
+import requests
 import os
-import random
-
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 import pandas as pd
-import datetime as dt
-import smtplib
+proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
 
-# 1. Update the birthdays.csv
-data = pd.read_csv('birthdays.csv')
-#birth_list = data.to_dict('records')
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-#print(birth_list)
+api_key = os.environ.get("OWM_API_KEY")
+lat = "48.941132"
+lon = "-57.971130"
 
-now = dt.datetime.now()
-#today_date = now.date()
-today_date = now.day
-today_month = now.month
-print(today_date)
-print(today_month)
+parameters = {
+    "appid": api_key,
+    "lat": lat,
+    "lon": lon,
+    "cnt":4,
+}
 
+response = requests.get(url=f"https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(response.status_code)
+print(data)
 
-
-my_email = os.environ.get("MY_EMAIL")
-password = os.environ.get("MY_PASSWORD")
-
-
-
-with open("./letter_templates/letter_1.txt", "r") as f:
-    letter_1 = f.read()
-
-with open("./letter_templates/letter_2.txt", "r") as f:
-    letter_2 = f.read()
-
-with open("./letter_templates/letter_3.txt", "r") as f:
-    letter_3 = f.read()
-
-letter_list = [letter_1, letter_2, letter_3]
-
-def send_email(to_address,email_content):
-    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-        connection.starttls()
-        connection.login(my_email, password)
-        connection.sendmail(to_addrs=email,
-                            from_addr=my_email,
-                            msg=f"Subject: Hello World!\n\n{email_content}"
-                            )
-
-
-# 2. Check if today matches a birthday in the birthdays.csv
-#
-# if today_date in birth_list["day"].values and today_month in birth_list["month"].values:
-#     birthday_person = birth_list["name"]
-#     print(birthday_person)
-
-birthday_person_row = data[(today_month == data["month"]) & (today_date == data["day"])]
-if not birthday_person_row.empty:
-    name = birthday_person_row["name"].item()
-    email = birthday_person_row["email"].item()
-    picked_letter = random.choice(letter_list)
-    final_letter = picked_letter.replace("[NAME]", name)
-    #print(picked_letter)
-
-    send_email(to_address=email, email_content=final_letter)
-
-# for birth in birth_list:
-#     if :
-#        birthday_person = birth_list[birth]["name"]
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
+will_rain = False
+for id in data["list"]:
+    id_number = id["weather"][0]["id"]
+    print(id_number)
+    if int(id_number) < 700:
+        will_rain= True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body ="Bring an Umbrella",
+        from_='whatsapp:+14155238886',
+        to='whatsapp:+19122248996'
+    )
+    print(message.status)
+    #print("Bring an Umbrella")
